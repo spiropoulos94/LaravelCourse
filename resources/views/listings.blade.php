4546f7ffdd61e179a7a16a24ec4459396c296a30
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2022-05-07 15:40:13 +0300

    fsaf

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,6 +1,7 @@
 @extends("layout")
 
 @section("content")
+@include('partials._hero')
 
 <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
     @unless(count($listings) == 0)
@@ -9,10 +10,10 @@
 
     <div class="bg-gray-50 border border-gray-200 rounded p-6">
         <div class="flex">
-            <img class="hidden w-48 mr-6 md:block" src="images/acme.png" alt="" />
+            <img class="hidden w-48 mr-6 md:block" src="{{asset('images/no-image.png')}}" alt="" />
             <div>
                 <h3 class="text-2xl">
-                    <a href="show.html">{{$listing->title}}</a>
+                    <a href="listings/{{$listing->id}}">{{$listing->title}}</a>
                 </h3>
                 <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
                 <ul class="flex">
