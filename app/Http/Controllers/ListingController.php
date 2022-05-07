9008b6c4d816041c94e727f9e6f4911573091650
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2022-05-07 20:53:07 +0300

    test

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -10,9 +10,10 @@ class ListingController extends Controller
     // show all listings
     public function index()
     {
+
         return view('listings.index', [
             "heading" => "Latest Listings",
-            "listings" => Listing::all(),
+            "listings" => Listing::latest()->filter(request(['tag']))->get(),
         ]);
     }
 
--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Listing extends Model
 {
     use HasFactory;
+
+    public function scopeFilter($query, array $filters)
+    {
+        dd($filters);
+
+        if ($filters['tag'] ?? false) { // an einai true tha gyrisei to tag, alliws false
+            $query->where('tags', 'like', '%' . request('tag') . '%');
+        }
+    }
 }
