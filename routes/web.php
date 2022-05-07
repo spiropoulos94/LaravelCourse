a5e62b58876511a273b7687f7f4c775f56cd35c1
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2022-05-07 17:58:20 +0300

    test

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,13 @@ Route::get('/', [ListingController::class, 'index']);
 // Single listing
 
 Route::get('/listings/{listing}',  [ListingController::class, 'show']);
+
+// Common Resource Routes : 
+
+// index - Show all listings
+// show - Show single listing
+// create - Show form to create new listing
+// store - Store new listing
+// edit - Show form to edit listing
+// update - Update listing
+// destroy - Delete listing
