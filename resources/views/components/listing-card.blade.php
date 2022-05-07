a6b8c9dadb4b55108e91ade3010d808ebf06fad0
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2022-05-07 17:45:02 +0300

    test

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -11,6 +11,8 @@
             </h3>
             <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
 
+            <x-listing-tags :tagsCsv="$listing->tags" />
+
             <div class="text-lg mt-4">
                 <i class="fa-solid fa-location-dot"></i> {{$listing->location}}
             </div>
