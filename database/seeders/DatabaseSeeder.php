245b36cc080ac406815cb68208bf3d054040470e
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2022-05-07 15:24:47 +0300

    fgasfas

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Listing;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+        \App\Models\Listing::factory(6)->create();
+    }
+}
