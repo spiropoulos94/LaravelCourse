2ebe342d90ae8f3e9d1363f9085c50da7daa810c
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2022-05-08 19:41:06 +0300

    test

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -59,7 +59,7 @@ class ListingController extends Controller
         ]);
 
         if ($request->hasFile('logo')) {
-            $formFields['logo'] = "fasgbajsgbliasdgasd";
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
         }
 
         // $formFields['logo'] = "skata";
