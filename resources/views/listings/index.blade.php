9199dfc6d0e7cc4483112772626f5b8bf85182c4
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2022-05-08 15:54:12 +0300

    test

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -1,7 +1,6 @@
-@include('partials._hero')
-@include('partials._search')
-
 <x-layout>
+    @include('partials._hero')
+    @include('partials._search')
     <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
         @unless(count($listings) == 0)
 
