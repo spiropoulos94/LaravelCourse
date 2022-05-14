8b05647483b7012584b95a05fb12b81ef119092e
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2022-05-14 18:40:41 +0300

    test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use App\Models\Listing;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -16,6 +17,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        \App\Models\Listing::factory(6)->create();
+        $user = User::factory()->create([
+            "name" => "John Doe",
+            "email" => "john@gmail.com"
+        ]);
+
+        \App\Models\Listing::factory(6)->create([
+            "user_id" => $user->id
+        ]);
     }
 }
